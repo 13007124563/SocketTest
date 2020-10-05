209a7a17f282aa6d26914273c92a0d9cfe96fc0c
fangcai <caifang@upchina.com>
2020-10-05 14:29:46 +0800

    Initial Commit

---
+++ SocketTest/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  SocketTest
+//
+//  Created by fang on 2020/10/5.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ SocketTest/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  SocketTest
+//
+//  Created by fang on 2020/10/5.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
